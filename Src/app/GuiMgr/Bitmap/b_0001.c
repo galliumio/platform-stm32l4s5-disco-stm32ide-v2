7c2cb0f009b84fdb8ccacb6563e2630625ad766a
admin <admin@galliumstudio.com>
2022-08-27 01:30:25 -0700

    Updated copyright notice.

--- Src/app/GuiMgr/Bitmap/b_0001.c
+++ Src/app/GuiMgr/Bitmap/b_0001.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2022 Gallium Studio LLC (Lawrence Lo). All rights reserved.
+ * Copyright (C) 2018 Gallium Studio LLC (Lawrence Lo). All rights reserved.
  *
  * This program is open source software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
